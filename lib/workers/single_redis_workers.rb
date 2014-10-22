d0f026e40a0656d56ce07aed69dfb6c533e7029d
danko-progress <danko-progress@progress-hp.(none)>
2014-10-22 21:05:53 +0400

    edit bug fix

--- lib/workers/single_redis_workers.rb
+++ lib/workers/single_redis_workers.rb
@@ -4,23 +4,23 @@ module SingleRedisWorkers
     sidekiq_options queue: :scheduled_jobs
 
     def perform
-      p "Clock worked"
+      p "Single task worked"
 
       @current_logger = Logger.new("#{File.dirname(__FILE__)}/../../log/single_sidekiq_worker_#{ENV['APP_ENV']}.log")
       @current_logger.info "NOTIFICATIONS: Started"      
            
 
-      last_obd = Db::OnBoardDevice.all
+      p last_obd = Db::OnBoardDevice.all
       if last_obd.present?
         move_to_redis(last_obd)
       end
 
-      last_trucks = Db::Truck.all
+      p last_trucks = Db::Truck.all
       if last_trucks.present?
         move_to_redis(last_trucks)
       end
 
-      last_obd = Db::CompanyAccount.all
+      p last_obd = Db::CompanyAccount.all
       if last_obd.present?
         move_to_redis(last_obd)
       end
@@ -31,16 +31,16 @@ module SingleRedisWorkers
 
     def move_to_redis(last_records)
       last_records.each do |obj|
-        obj_redis_name = obj.class.name.gsub("Db::", "").underscore.to_s
+        p obj_redis_name = obj.class.name.gsub("Db::", "").underscore.to_s
         # on_board_device хранятся по imei - это number в db
         if obj_redis_name == "on_board_device"
-          obj_id = obj.number
+          p obj_id = obj.number
         else
-          obj_id = obj.id
+          p obj_id = obj.id
         end
         h = Hash.new
         # убираем косяк с обратной конвертацией времени
-        obj.attributes.each {|key, value| h[key] = value.to_s }
+        p obj.attributes.each {|key, value| h[key] = value.to_s }
         $redis.set("#{$config['redis_cache']['prefix']}:#{obj_redis_name}:#{obj_id}", h)
       end
     end
